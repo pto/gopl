0a73679e254d11dbcc911c0fa564f03fa71c7d45
Peter Olsen <pto@me.com>
2015-11-21 09:05:55 -0500

    exercise 1-8

---
+++ ch01/ex08/main.go
@@ -0,0 +1,32 @@
+// Ex08 prints the content found at a URL, and adds a protocol if it is missing.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.Contains(url, ":") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ex08: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ex08: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
+
+//!-
