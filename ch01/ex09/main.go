d0687b821f730cc4b926912f47aa8990f222fd7b
Peter Olsen <pto@me.com>
2015-11-21 09:11:44 -0500

    Exercise 1-9

---
+++ ch01/ex09/main.go
@@ -0,0 +1,27 @@
+// Ex09 prints the content found at a URL and the HTTP status code.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ex09: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ex09: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+		fmt.Printf("\nStatus: %v\n", resp.Status)
+	}
+}
