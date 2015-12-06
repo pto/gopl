a58347c462590a6c611c76709fe5cf8a5f7ba914
Peter Olsen <pto@me.com>
2015-12-06 11:21:16 -0500

    Exercise 1.10

---
+++ ch01/ex10/main.go
@@ -0,0 +1,50 @@
+// Ex10 fetches URLs in parallel, reports their times and sizes, and saves
+// the output in a series of files.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for index, url := range os.Args[1:] {
+		go fetch(url, index, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, index int, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	filename := fmt.Sprintf("fetchall.%d", index)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+	if err != nil {
+		ch <- fmt.Sprintf("while opening %s: %v", filename, err)
+		return
+	}
+	nbytes, err := io.Copy(f, resp.Body)
+	resp.Body.Close() // don't leak resources
+	f.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
