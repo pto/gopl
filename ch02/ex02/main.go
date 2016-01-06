515dcb422d897a3d7fdb160e05896b9a29550894
Peter Olsen <pto@me.com>
2016-01-05 21:24:46 -0500

    adding standard input option

--- ch02/ex02/main.go
+++ ch02/ex02/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,30 +11,44 @@ import (
 )
 
 func main() {
-	for i, arg := range os.Args[1:] {
-		x, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ex02: %v\n", err)
-			os.Exit(1)
+	if len(os.Args) > 1 {
+		for i, arg := range os.Args[1:] {
+			display(arg)
+			if i < len(os.Args)-2 {
+				fmt.Println()
+			}
 		}
-		f := conv.Fahrenheit(x)
-		c := conv.Celsius(x)
-		fmt.Printf("%s = %s, %s = %s\n", f, conv.FToC(f), c, conv.CToF(c))
-
-		ft := conv.Feet(x)
-		m := conv.Meters(x)
-		fmt.Printf("%s = %s, %s = %s\n", ft, conv.FToM(ft), m, conv.MToF(m))
-
-		p := conv.Pounds(x)
-		k := conv.Kilograms(x)
-		fmt.Printf("%s = %s, %s = %s\n", p, conv.PToK(p), k, conv.KToP(k))
-
-		a := conv.Acres(x)
-		h := conv.Hectares(x)
-		fmt.Printf("%s = %s, %s = %s\n", a, conv.AToH(a), h, conv.HToA(h))
-
-		if i < len(os.Args)-2 {
-			fmt.Println()
+	} else {
+		s := bufio.NewScanner(os.Stdin)
+		s.Split(bufio.ScanWords)
+		var sep string
+		for s.Scan() {
+			fmt.Print(sep)
+			display(s.Text())
+			sep = "\n"
 		}
 	}
+}
+
+func display(val string) {
+	x, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ex02: %v\n", err)
+		os.Exit(1)
+	}
+	f := conv.Fahrenheit(x)
+	c := conv.Celsius(x)
+	fmt.Printf("%s = %s, %s = %s\n", f, conv.FToC(f), c, conv.CToF(c))
+
+	ft := conv.Feet(x)
+	m := conv.Meters(x)
+	fmt.Printf("%s = %s, %s = %s\n", ft, conv.FToM(ft), m, conv.MToF(m))
+
+	p := conv.Pounds(x)
+	k := conv.Kilograms(x)
+	fmt.Printf("%s = %s, %s = %s\n", p, conv.PToK(p), k, conv.KToP(k))
+
+	a := conv.Acres(x)
+	h := conv.Hectares(x)
+	fmt.Printf("%s = %s, %s = %s\n", a, conv.AToH(a), h, conv.HToA(h))
 }
