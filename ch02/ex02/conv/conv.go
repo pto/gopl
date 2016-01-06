764401ab4a69c342cc17c4bf329e4db9063a948d
Peter Olsen <pto@me.com>
2016-01-05 20:36:36 -0500

    adding units

--- ch02/ex02/conv/conv.go
+++ ch02/ex02/conv/conv.go
@@ -1,4 +1,4 @@
-// conv defines various units and conversions.
+// Package conv defines various units and conversions.
 package conv
 
 import "fmt"
@@ -6,10 +6,30 @@ import "fmt"
 type (
 	Celsius    float64
 	Fahrenheit float64
+	Meters     float64
+	Feet       float64
+	Kilograms  float64
+	Pounds     float64
+	Hectares   float64
+	Acres      float64
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func CToF(c Celsius) Fahrenheit     { return Fahrenheit(c*9/5 + 32) }
+func FToC(f Fahrenheit) Celsius     { return Celsius((f - 32) * 5 / 9) }
 
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func (m Meters) String() string { return fmt.Sprintf("%g m", m) }
+func (f Feet) String() string   { return fmt.Sprintf("%g ft", f) }
+func MToF(m Meters) Feet        { return Feet(m / 0.3048) }
+func FToM(f Feet) Meters        { return Meters(f * 0.3048) }
+
+func (k Kilograms) String() string { return fmt.Sprintf("%g kg", k) }
+func (p Pounds) String() string    { return fmt.Sprintf("%g lbs", p) }
+func KToP(k Kilograms) Pounds      { return Pounds(k * 1000 / 453.59237) }
+func PToK(p Pounds) Kilograms      { return Kilograms(p * 453.59237 / 1000) }
+
+func (h Hectares) String() string { return fmt.Sprintf("%g hectares", h) }
+func (a Acres) String() string    { return fmt.Sprintf("%g acres", a) }
+func HToA(h Hectares) Acres       { return Acres(h * 2.4710538148) }
+func AToH(a Acres) Hectares       { return Hectares(a / 2.4710538148) }
--- ch02/ex02/main.go
+++ ch02/ex02/main.go
@@ -10,15 +10,30 @@ import (
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
+	for i, arg := range os.Args[1:] {
+		x, err := strconv.ParseFloat(arg, 64)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			fmt.Fprintf(os.Stderr, "ex02: %v\n", err)
 			os.Exit(1)
 		}
-		f := conv.Fahrenheit(t)
-		c := conv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			f, conv.FToC(f), c, conv.CToF(c))
+		f := conv.Fahrenheit(x)
+		c := conv.Celsius(x)
+		fmt.Printf("%s = %s, %s = %s\n", f, conv.FToC(f), c, conv.CToF(c))
+
+		ft := conv.Feet(x)
+		m := conv.Meters(x)
+		fmt.Printf("%s = %s, %s = %s\n", ft, conv.FToM(ft), m, conv.MToF(m))
+
+		p := conv.Pounds(x)
+		k := conv.Kilograms(x)
+		fmt.Printf("%s = %s, %s = %s\n", p, conv.PToK(p), k, conv.KToP(k))
+
+		a := conv.Acres(x)
+		h := conv.Hectares(x)
+		fmt.Printf("%s = %s, %s = %s\n", a, conv.AToH(a), h, conv.HToA(h))
+
+		if i < len(os.Args)-2 {
+			fmt.Println()
+		}
 	}
 }
