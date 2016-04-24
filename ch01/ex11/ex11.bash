12e8cdb5fc41a7fa8f7fb48783f05834fcaa7ede
Peter Olsen <pto@me.com>
2016-04-24 08:17:15 -0400

    adding Exercise 1.11

---
+++ ch01/ex11/ex11.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+fetchall \
+	http://google.com \
+	http://youtube.com \
+	http://facebook.com \
+	http://baidu.com \
+	http://yahoo.com \
+	http://amazon.com \
+	http://wikipedia.org \
+	http://qq.com \
+	http://google.co.in \
+	http://twitter.com \
+	http://live.com \
+	http://taobao.com \
+	http://google.co.jp \
+	http://bing.com \
+	http://sina.com.cn \
+	http://msn.com \
+	http://yahoo.co.jp \
+	http://linkedin.com \
+	http://weibo.com \
+	http://vk.com \
+	http://instagram.com \
+	http://yandex.ru \
+	http://hao123.com \
+	http://google.de \
+	http://google.ru \
+	\
+	http://gannett.co
