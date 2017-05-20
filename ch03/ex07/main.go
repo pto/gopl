39aeba089357b2d592ec2d2f7662e1221044166a
Peter Olsen <pto@me.com>
2017-05-20 14:54:46 -0400

    Exercise 3.7

--- ch03/ex07/main.go
+++ ch03/ex07/main.go
@@ -1,4 +1,4 @@
-// Ex05 emits a PNG image of the Mandelbrot fractal in color.
+// Ex07 emits a PNG image of the solutions to z^4-1=0.
 package main
 
 import (
@@ -22,22 +22,32 @@ func main() {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, newton(z))
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
 }
 
-func mandelbrot(z complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
+// Generate a color based on the number of iterations needed to come
+// close to a root to z^4-1=0 using Newton's method.
+//
+// Improve guess x0 by calculating x1=x0-f(x0)/f'(x0)
+//                                   =x0-(x0^4-1)/(4*x0^3)
+//                                   =x0-x0/4+1/(4*x0^3)
+func newton(z complex128) color.Color {
+	const (
+		limit    = 50
+		contrast = 20
+		epsilon  = 1e-6
+	)
 
-	var v complex128
-	for n := uint8(0); n < iterations; n++ {
-		v = v*v + z
-		if cmplx.Abs(v) > 2 {
+	x0 := z
+	for n := uint8(0); n < limit; n++ {
+		x1 := x0 - x0/4 + 1/(4*x0*x0*x0)
+		if cmplx.Abs(x1*x1*x1*x1-1) < epsilon {
 			return color.YCbCr{128, 255 - contrast*n, contrast*n - 255}
 		}
+		x0 = x1
 	}
 	return color.Black
 }
