401035bc5ef438c011c871859ef4d5ddc4ecdaa2
Peter Olsen <pto@me.com>
2017-07-03 18:31:14 -0400

    Exercise 4.3

---
+++ ch04/ex03/main.go
@@ -0,0 +1,19 @@
+// Ex03 reverses an array
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := [6]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+}
+
+// reverse reverses an array of ints in place.
+func reverse(a *[6]int) {
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
