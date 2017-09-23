37d348076b2d2cee04ac52b5349ccf46fab18d20
Peter Olsen <pto@me.com>
2017-09-23 16:55:13 -0400

    slight cleanup

--- ch01/ex06/main.go
+++ ch01/ex06/main.go
@@ -14,19 +14,19 @@ import (
 
 var palette = []color.Color{color.Black}
 
-const paletteSize = 256
+const paletteSize = math.MaxUint8
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	// 255/3 is exactly 85, so each loop adds 85 entries
-	for i := 1; i < paletteSize; i += 3 {
+	for i := 0; i < paletteSize; i += 3 {
 		addToPalette(paletteSize-i, i, 0) // Red turning to Green
 	}
-	for i := 1; i < paletteSize; i += 3 {
+	for i := 0; i < paletteSize; i += 3 {
 		addToPalette(0, paletteSize-i, i) // Green turning to Blue
 	}
-	for i := 1; i < paletteSize; i += 3 {
+	for i := 0; i < paletteSize; i += 3 {
 		addToPalette(i, 0, paletteSize-i) // Blue turning to Red
 	}
 
@@ -48,14 +48,14 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
-	rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			// Range from 1 to paletteSize-1 (use full palette exactly once)
-			paletteIndex := uint8(t*(paletteSize-1)/(cycles*2*math.Pi)) + 1
+			// Range from 1 to paletteSize (use full palette exactly once)
+			paletteIndex := uint8(t*paletteSize/(cycles*2*math.Pi)) + 1
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
 				paletteIndex)
 		}
