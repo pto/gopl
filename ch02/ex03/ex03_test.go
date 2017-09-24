73403e030d668c538e1484456875da00d3f0d025
Peter Olsen <pto@me.com>
2017-09-24 08:18:17 -0400

    benchmark and test with more values

--- ch02/ex03/ex03_test.go
+++ ch02/ex03/ex03_test.go
@@ -5,56 +5,72 @@ import (
 	"testing"
 )
 
-const testWord uint64 = 0x1234567890abcdef
+var testWords = [...]uint64{0x1234567890abcdef, 0x0, 0x800000000,
+	0x8000000000000000, 0xFFFFFFFFFFFFFFFF}
 
 func TestPopCounts(t *testing.T) {
-	expected := PopCount(testWord)
-	check := func(f string, result int) {
-		if result != expected {
-			t.Fatalf("%s: %d does not match expected result %d",
-				f, result, expected)
+	check := func(name string, f func(uint64) int) {
+		for _, v := range testWords {
+			result := f(v)
+			expected := PopCount(v)
+			if result != expected {
+				t.Fatalf("%s: %d does not match expected result %d",
+					name, result, expected)
+			}
 		}
 	}
 
-	check("PopCountLoop1", PopCountLoop1(testWord))
-	check("PopCountLoop2", PopCountLoop2(testWord))
-	check("PopCountShift", PopCountShift(testWord))
-	check("PopCountMask", PopCountMask(testWord))
-	check("OnesCount", bits.OnesCount64(testWord))
+	check("PopCountLoop1", PopCountLoop1)
+	check("PopCountLoop2", PopCountLoop2)
+	check("PopCountShift", PopCountShift)
+	check("PopCountMask", PopCountMask)
+	check("OnesCount64", bits.OnesCount64)
 }
 
 func BenchmarkPopCount(b *testing.B) {
 	for i := 1; i < b.N; i++ {
-		PopCount(testWord)
+		for _, v := range testWords {
+			PopCount(v)
+		}
 	}
 }
 
 func BenchmarkPopCountLoop1(b *testing.B) {
 	for i := 1; i < b.N; i++ {
-		PopCountLoop1(testWord)
+		for _, v := range testWords {
+			PopCountLoop1(v)
+		}
 	}
 }
 
 func BenchmarkPopCountLoop2(b *testing.B) {
 	for i := 1; i < b.N; i++ {
-		PopCountLoop2(testWord)
+		for _, v := range testWords {
+			PopCountLoop2(v)
+		}
 	}
 }
 
 func BenchmarkPopCountShift(b *testing.B) {
 	for i := 1; i < b.N; i++ {
-		PopCountShift(testWord)
+		for _, v := range testWords {
+			PopCountShift(v)
+		}
 	}
 }
 
 func BenchmarkPopCountMask(b *testing.B) {
 	for i := 1; i < b.N; i++ {
-		PopCountMask(testWord)
+		for _, v := range testWords {
+			PopCountMask(v)
+		}
 	}
 }
 
 func BenchmarkOnesCount(b *testing.B) {
 	for i := 1; i < b.N; i++ {
-		bits.OnesCount64(testWord)
+		for _, v := range testWords {
+			bits.OnesCount64(v)
+		}
 	}
 }
