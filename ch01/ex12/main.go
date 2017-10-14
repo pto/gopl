d6c371a0d36b2190e091465da17e815c72f70e9b
Peter Olsen <pto@me.com>
2017-10-14 19:55:26 -0400

    keep close to original lissajous program

--- ch01/ex12/main.go
+++ ch01/ex12/main.go
@@ -1,10 +1,9 @@
-// Ex12 serves GIF animations of random Lissajous curves in multiple colors,
-// accepting query parameters for frequency, phase step, cycles, number of
-// frames, delay between frames, image size and angular resolution.
+// Ex12 serves GIF animations of random Lissajous figures, accepting query
+// parameters for cycles, angular resolution, image size, number of frames,
+// delay between frames, y-axis frequency, and y-axis phase step.
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
@@ -17,31 +16,17 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.Black}
+var palette = []color.Color{color.White, color.Black}
 
-const paletteSize = math.MaxUint8
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	fillPalette()
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
-
-func fillPalette() {
-	addToPalette := func(r, g, b int) {
-		palette = append(palette, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
-	}
-
-	for i := 0; i < paletteSize; i += 3 {
-		addToPalette(paletteSize-i, i, 0) // Red turning to Green
-	}
-	for i := 0; i < paletteSize; i += 3 {
-		addToPalette(0, paletteSize-i, i) // Green turning to Blue
-	}
-	for i := 0; i < paletteSize; i += 3 {
-		addToPalette(i, 0, paletteSize-i) // Blue turning to Red
-	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -50,17 +35,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 		return
 	}
-	freq, err := strconv.ParseFloat(r.Form.Get("freq"), 64)
-	if err != nil {
-		freq = rand.Float64() * 3.0
-	}
-	phaseStep, err := strconv.ParseFloat(r.Form.Get("phaseStep"), 64)
-	if err != nil {
-		phaseStep = 0.1
-	}
-	cycles, err := strconv.ParseFloat(r.Form.Get("cycles"), 64)
+	cycles, err := strconv.Atoi(r.Form.Get("cycles"))
 	if err != nil {
 		cycles = 5
+	}
+	res, err := strconv.ParseFloat(r.Form.Get("res"), 64)
+	if err != nil {
+		res = 0.001
+	}
+	size, err := strconv.Atoi(r.Form.Get("size"))
+	if err != nil {
+		size = 100
 	}
 	nframes, err := strconv.Atoi(r.Form.Get("nframes"))
 	if err != nil {
@@ -70,33 +55,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		delay = 8
 	}
-	size, err := strconv.Atoi(r.Form.Get("size"))
+	freq, err := strconv.ParseFloat(r.Form.Get("freq"), 64)
 	if err != nil {
-		size = 100
+		freq = rand.Float64() * 3.0
 	}
-	res, err := strconv.ParseFloat(r.Form.Get("res"), 64)
+	phaseStep, err := strconv.ParseFloat(r.Form.Get("phaseStep"), 64)
 	if err != nil {
-		res = 0.001
+		phaseStep = 0.1
 	}
-	lissajous(w, freq, phaseStep, cycles, nframes, delay, size, res)
+	lissajous(w, cycles, res, size, nframes, delay, freq, phaseStep)
 }
 
-func lissajous(out io.Writer, freq float64, phaseStep float64, cycles float64,
-	nframes int, delay int, size int, res float64) {
-	fmt.Printf("freq=%g, phaseStep=%g, cycles=%g, nframes=%d, delay=%d, "+
-		"size=%d, res=%g\n", freq, phaseStep, cycles, nframes, delay, size, res)
+func lissajous(out io.Writer, cycles int, res float64, size int, nframes int,
+	delay int, freq float64, phaseStep float64) {
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
-	rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		upperLimit := cycles * 2 * math.Pi
-		for t := 0.0; t < upperLimit; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*float64(size)+0.5),
-				size+int(y*float64(size)+0.5),
-				uint8(t*paletteSize/upperLimit)+1)
+				size+int(y*float64(size)+0.5), blackIndex)
 		}
 		phase += phaseStep
 		anim.Delay = append(anim.Delay, delay)
