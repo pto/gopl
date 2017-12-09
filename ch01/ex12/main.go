d7b7e424b167e63669b77d29821782734845658c
Peter Olsen <pto@me.com>
2017-12-08 20:09:48 -0500

    simplify by only allowing integral parameters

--- ch01/ex12/main.go
+++ ch01/ex12/main.go
@@ -1,6 +1,6 @@
 // Ex12 serves GIF animations of random Lissajous figures, accepting query
-// parameters for cycles, angular resolution, image size, number of frames,
-// delay between frames, y-axis frequency, and y-axis phase step.
+// parameters for cycles, image size, number of frames, and delay between
+// frames.
 package main
 
 import (
@@ -38,10 +38,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		cycles = 5
 	}
-	res, err := strconv.ParseFloat(r.Form.Get("res"), 64)
-	if err != nil {
-		res = 0.001
-	}
 	size, err := strconv.Atoi(r.Form.Get("size"))
 	if err != nil {
 		size = 100
@@ -54,19 +50,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		delay = 8
 	}
-	freq, err := strconv.ParseFloat(r.Form.Get("freq"), 64)
-	if err != nil {
-		freq = rand.Float64() * 3.0
-	}
-	phaseStep, err := strconv.ParseFloat(r.Form.Get("phaseStep"), 64)
-	if err != nil {
-		phaseStep = 0.1
-	}
-	lissajous(w, cycles, res, size, nframes, delay, freq, phaseStep)
+	lissajous(w, cycles, size, nframes, delay)
 }
 
-func lissajous(out io.Writer, cycles int, res float64, size int, nframes int,
-	delay int, freq float64, phaseStep float64) {
+func lissajous(out io.Writer, cycles int, size int, nframes int, delay int) {
+	const res = 0.001            // angular resolution
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
@@ -78,7 +67,7 @@ func lissajous(out io.Writer, cycles int, res float64, size int, nframes int,
 			img.SetColorIndex(size+int(x*float64(size)+0.5),
 				size+int(y*float64(size)+0.5), blackIndex)
 		}
-		phase += phaseStep
+		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
