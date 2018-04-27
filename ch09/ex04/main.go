c13d69f7479449aac31b9de6633b4851f4454a6e
Peter Olsen <pto@me.com>
2018-04-27 19:59:49 -0400

    goroutine pipeline

---
+++ ch09/ex04/main.go
@@ -0,0 +1,45 @@
+// Ex04 creates a chain of goroutines connected by channels and prints
+// the time needed to transit the entire pipeline.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func stage(in <-chan int, out chan<- int) {
+	out <- (<-in + 1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		usage()
+	}
+
+	root := make(chan int)
+	next := root
+	start := time.Now()
+	for i := 0; i < n; i++ {
+		prev := next
+		next = make(chan int)
+		go stage(prev, next)
+	}
+	fmt.Println("Starting goroutines:", time.Since(start))
+
+	start = time.Now()
+	root <- 0
+	fmt.Println("Transiting pipeline:", time.Since(start))
+	fmt.Println("Result:", <-next)
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage: ex04 <number of goroutines>")
+	os.Exit(1)
+}
